4cc97b1ad1f989d8e198f94cf8c46d5e70151814
Rubén Arroyo <rusben@gmail.com>
2013-11-24 14:14:13 +0100

    Added error handling | Reestructuring app

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,19 @@
-var AppRouter = Backbone.Router.extend({
+var app = {
+
+	views : {},
+
+	models : {},
+	
+	content : function() {
+		return $("#content");
+	},
+	
+	loggedIn : function() {
+		return !($.cookie('userId') === null || $.cookie('userId') === undefined);
+	}
+};
+
+app.Router = Backbone.Router.extend({
 
     routes: {
         ""                  : "catalogue",
@@ -15,7 +30,7 @@ var AppRouter = Backbone.Router.extend({
         var p = page ? parseInt(page, 10) : 1;
         var plantList = new PlantCollection();
         plantList.fetch({success: function(){
-            $("#content").html(new CatalogueView({model: plantList, page: p}).el);
+            app.content().html(new CatalogueView({model: plantList, page: p}).el);
         }});
 //        this.headerView.selectMenuItem('home-menu');
     },
@@ -23,15 +38,42 @@ var AppRouter = Backbone.Router.extend({
     details: function (id) {
         var plant = new Plant({id: id});
         plant.fetch({success: function(model, response){
-            $("#content").html(new DetailsView({model: model}).el);
+        	app.content().html(new DetailsView({model: model}).el);
         }});
 //        this.headerView.selectMenuItem();
     }
 
-
 });
 
-utils.loadTemplate(['HeaderView','CatalogueView','CatalogueItemView','DetailsView'], function() {
-    app = new AppRouter();
-    Backbone.history.start();
-});
+$(document).ready(
+		function() {
+			utils.loadTemplates(['HeaderView','CatalogueView','CatalogueItemView','DetailsView'], function() {
+				app.router = new app.Router();
+				Backbone.history.start();
+			});
+
+			// Tell jQuery to watch for any 401 or 403 errors and handle them
+			// appropriately
+			$.ajaxSetup( {
+				statusCode : {
+					401 : function() {
+						// Redirect the to the login page.
+						app.router.navigate('sign-in', {
+							trigger : true
+						});
+					},
+					403 : function() {
+						// 403 -- Access denied
+						app.router.navigate('access-denied', {
+							trigger : true
+						});
+					},
+					404 : function() {
+						// 404 -- File not found
+						app.router.navigate('page-not-found', {
+							trigger : true
+						});
+					}
+				}
+			});
+		});
